5cad0fd7f9b5567bd20816d2a4190246e1728648
Rutger Swarts <rswarts@hostnet.nl>
2015-09-15 03:27:14 +0200

    Update wrapper

--- bin/puppetdb.sh
+++ bin/puppetdb.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Wrap the wrappables...
 PATH="/usr/local/bin:$PATH"
-eval "$(plenv init -)"
-pushd /opt/puppetdb-stencils > /dev/null 2>&1
+pushd /opt/puppetdb.pl > /dev/null 2>&1
 carton exec -- ./bin/puppetdb.pl "$@"
 popd > /dev/null 2>&1
